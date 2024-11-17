4cee9c80ff78e63ca50c0a9cdec01811aeeac369
Hannes Reinecke <hare@suse.de>
2024-11-17 11:30:05 +0100

    Fixup xdp filter

--- xdp_drop_port.c
+++ xdp_drop_port.c
@@ -1,3 +1,11 @@
+/*
+ * xdp_drop_port.c
+ *
+ * Drop packets for port '4420' on localhost.
+ *
+ * Load with:
+ * ip link set dev lo xdpgeneric obj xdp_drop_port.o sec xdp_drop
+ */
 #include <stdio.h>
 #include <inttypes.h>
 #include <netinet/in.h>
@@ -6,9 +14,10 @@
 #include <linux/tcp.h>
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
 
-SEC("xdp")
-int xdp_drop(struct xdp_md *ctx)
+SEC("xdp_drop")
+int xdp_drop_port(struct xdp_md *ctx)
 {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
@@ -16,17 +25,18 @@ int xdp_drop(struct xdp_md *ctx)
     struct iphdr *i;
     struct tcphdr *t;
     __u16 h_proto;
+    __u16 t_dest, t_source;
 
     if (data + sizeof(struct ethhdr) > data_end)
         return XDP_PASS;
 
-    h_proto = ntohs(e->h_proto);
+    h_proto = bpf_ntohs(e->h_proto);
     if (h_proto != ETH_P_IP)
         return XDP_PASS;
 
     i = data + sizeof (struct ethhdr);
     if (i + 1 > (struct iphdr *)data_end)
-        return XDP_PASS;
+        return XDP_DROP;
 
     if (i->protocol != IPPROTO_TCP)
         return XDP_PASS;
@@ -34,10 +44,17 @@ int xdp_drop(struct xdp_md *ctx)
     t = data + sizeof(struct ethhdr) + (i->ihl * 4);
 
     if (t + 1 > (struct tcphdr *)data_end)
-        return XDP_PASS;
+        return XDP_DROP;
 
-    if (t->source == htons(4420) || t->dest == htons(4420))
-	return XDP_DROP;
+    /* Filter out SQE PDUs */
+    t_dest = bpf_ntohs(t->dest);
+    if (t_dest == 4420)
+	    return XDP_DROP;
+    /* Filter out CQE PDUs */
+    t_source = bpf_ntohs(t->source);
+    if (t_source == 4420)
+	    return XDP_DROP;
+
     return XDP_PASS;
 }
 char _license[] SEC("license") = "GPL";
