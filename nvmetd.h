a69a44a717adca4d815f408b3bd79e46b3ae2ed5
Hannes Reinecke <hare@suse.de>
2024-12-14 13:11:10 +0100

    Add missing header file

---
+++ nvmetd.h
@@ -0,0 +1,17 @@
+#ifndef _NVMETD_H
+#define _NVMETD_H
+
+extern bool inotify_debug;
+
+struct watcher_ctx {
+	struct etcd_ctx *etcd;
+	char *pathname;
+	int path_fd;
+	int inotify_fd;
+};
+
+void *inotify_loop(void *arg);
+int start_inotify(struct watcher_ctx *ctx);
+void stop_inotify(struct watcher_ctx *ctx);
+
+#endif /* _NVMETD_H */
