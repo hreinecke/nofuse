3d84ccaca49ee0f176a708845f9866934ca865f5
Hannes Reinecke <hare@suse.de>
2025-01-08 08:16:24 +0100

    nofuse: add keepalive loop

--- daemon.c
+++ daemon.c
@@ -39,6 +39,7 @@ bool ep_debug;
 bool port_debug;
 
 struct nofuse_context {
+	pthread_mutex_t etcd_mutex;
 	struct etcd_ctx *etcd;
 	char *subsysnqn;
 	const char *traddr;
@@ -46,6 +47,7 @@ struct nofuse_context {
 	const char *prefix;
 	int debug;
 	int help;
+	int ttl;
 };
 
 char discovery_nqn[MAX_NQN_SIZE + 1] = {};
@@ -99,7 +101,23 @@ static int init_subsys(struct nofuse_context *ctx)
 	return 0;
 }
 
-#define OPTION(t, p)				\
+static void *keepalive_loop(void *arg)
+{
+	struct nofuse_context *ctx = arg;
+	int ret;
+
+	while (!stopped) {
+		pthread_mutex_lock(&ctx->etcd_mutex);
+		ret = etcd_lease_keepalive(ctx->etcd);
+		pthread_mutex_unlock(&ctx->etcd_mutex);
+		if (ret < 0)
+			break;
+		sleep(ctx->etcd->ttl >> 1);
+	}
+	return NULL;
+}
+
+#define OPTION(t, p)					\
     { t, offsetof(struct nofuse_context, p), 1 }
 
 static const struct fuse_opt nofuse_options[] = {
@@ -109,6 +127,7 @@ static const struct fuse_opt nofuse_options[] = {
 	OPTION("--traddr=%s", traddr),
 #ifdef NOFUSE_ETCD
 	OPTION("--prefix=%s", prefix),
+	OPTION("--ttl=%d", ttl),
 #else
 	OPTION("--dbname=%s", dbname),
 #endif
@@ -124,6 +143,7 @@ static void show_help(void)
 	printf("  --subsysnqn=<NQN> - Discovery subsystem NQN to use");
 #ifdef NOFUSE_ETCD
 	printf("  --prefix=<prefix> - etcd key-value prefix");
+	printf("  --ttl=<ttl> - Time-to-live for etcd key-value pairs");
 #else
 	printf("  --dbname=<filename> - Database filename");
 #endif
@@ -168,11 +188,13 @@ int main(int argc, char *argv[])
 	int ret = 1;
 	struct nofuse_context *ctx;
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+	pthread_t keepalive_thr;
 
 	ctx = malloc(sizeof(struct nofuse_context));
 	if (!ctx)
 		return 1;
 	memset(ctx, 0, sizeof(struct nofuse_context));
+	pthread_mutex_init(&ctx->etcd_mutex, NULL);
 #ifndef NOFUSE_ETCD
 	ctx->dbname = strdup("nofuse.sqlite");
 #endif
@@ -190,6 +212,7 @@ int main(int argc, char *argv[])
 
 #ifdef NOFUSE_ETCD
 	ctx->etcd = etcd_init(ctx->prefix);
+	ctx->etcd->ttl = ctx->ttl;
 	ret = etcd_lease_grant(ctx->etcd);
 #else
 	ret = configdb_open(ctx->dbname);
@@ -203,9 +226,19 @@ int main(int argc, char *argv[])
 
 	stopped = 0;
 
+	ret = pthread_create(&keepalive_thr, NULL, keepalive_loop, ctx);
+	if (ret) {
+		fprintf(stderr, "cannot start keepalive loop\n");
+		goto out_stop;
+	}
+
 	run_fuse(&args, ctx->etcd);
 
+out_stop:
 	stopped = 1;
+
+	pthread_cancel(keepalive_thr);
+	pthread_join(keepalive_thr, NULL);
 
 out_close:
 #ifdef NOFUSE_ETCD
