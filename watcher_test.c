d5eb10b59099b2a257890534a89376d055790d62
Hannes Reinecke <hare@suse.de>
2025-01-10 11:24:31 +0100

    watcher_test: finalize initial test

--- watcher_test.c
+++ watcher_test.c
@@ -8,10 +8,42 @@
 #include <netdb.h>
 #include <errno.h>
 
-static int etcd_connect(void)
+#include <json-c/json.h>
+
+#include "base64.h"
+
+static char *default_hostname = "localhost";
+static char *default_port = "2379";
+
+static char *__b64enc(const char *str, int str_len)
 {
-	const char *hostname = "localhost";
-	const char *port = "2379";
+	int encoded_size = (str_len * 2) + 2, len;
+	char *encoded_str = malloc(encoded_size + 1);
+
+	if (!encoded_str)
+		return NULL;
+	memset(encoded_str, 0, encoded_size);
+	len = base64_encode((unsigned char *)str, str_len, encoded_str);
+	encoded_str[len] = '\0';
+	return encoded_str;
+}
+
+static char *__b64dec(const char *encoded_str)
+{
+	int encoded_size = strlen(encoded_str), len;
+	char *str = malloc(encoded_size + 1);
+
+	if (!str)
+		return NULL;
+
+	memset(str, 0, encoded_size);
+	len = base64_decode(encoded_str, encoded_size, (unsigned char *)str);
+	str[len] = '\0';
+	return str;
+}
+
+static int etcd_connect(char *hostname, char *port)
+{
 	struct addrinfo hints, *ai, *aip;
 	int sockfd = -1, ret;
 
@@ -37,11 +69,16 @@ static int etcd_connect(void)
 			fprintf(stderr, "socket error %d\n", errno);
 			continue;
 		}
-		if (connect(sockfd, aip->ai_addr, ai->ai_addrlen) == 0) {
-			printf("connected\n");
+		if (connect(sockfd, aip->ai_addr, aip->ai_addrlen) == 0) {
+			const char *fam = "IPv4";
+
+			if (aip->ai_family == AF_INET6)
+				fam = "IPv6";
+
+			printf("connected to %s:%s with %s\n",
+			       hostname, port, fam);
 			break;
 		}
-		printf("connection failed, error %d\n", errno);
 		close(sockfd);
 		sockfd = -ENOTCONN;
 	}
@@ -51,18 +88,41 @@ static int etcd_connect(void)
 	return sockfd;
 }
 
-const char http_header[] =
-	"POST /v3/watch HTTP/1.1\r\n"
-	"Host: localhost:2379\r\n"
-	"Accept: */*\r\n"
-	"Content-Type: application/json\r\n"
-	"Content-Length: 113\r\n\r\n";
+char *format_watch(char *key, int64_t revision, int64_t watch_id)
+{
+	json_object *post_obj, *req_obj;
+	char *encoded_key, end, *end_key, *encoded_end;
+	const char *tmp;
+	char *buf;
 
-const char http_post_watch[] =
-	"{ \"create_request\": "
-	"{ \"key\": \"bm9mdXNlL3BvcnRz\", "
-	"\"range_end\": \"bm9mdXNlL3BvcnR0\", "
-	"\"watch_id\": 140095673849537 } }";
+	post_obj = json_object_new_object();
+	req_obj = json_object_new_object();
+	encoded_key = __b64enc(key, strlen(key));
+	json_object_object_add(req_obj, "key",
+			       json_object_new_string(encoded_key));
+	end_key = strdup(key);
+	end = end_key[strlen(end_key) - 1];
+	end++;
+	end_key[strlen(end_key) - 1] = end;
+	encoded_end = __b64enc(end_key, strlen(end_key));
+	json_object_object_add(req_obj, "range_end",
+			       json_object_new_string(encoded_end));
+	if (revision > 0)
+		json_object_object_add(req_obj, "start_revision",
+				       json_object_new_int64(revision));
+	if (watch_id > 0)
+		json_object_object_add(req_obj, "watch_id",
+				       json_object_new_int64(watch_id));
+	json_object_object_add(post_obj, "create_request", req_obj);
+
+	tmp = json_object_to_json_string_ext(post_obj,
+					     JSON_C_TO_STRING_PRETTY);
+
+	buf = strdup(tmp);
+	json_object_put(post_obj);
+	free(end_key);
+	return buf;
+}
 
 const char http_post_cancel[] =
 	"{ \"cancel_request\": "
@@ -92,6 +152,12 @@ int send_http(int sockfd, const char *data, size_t data_len)
 		data_ptr += len;
 	}
 	return data_left;
+}
+
+int parse_http(char *buf, size_t len)
+{
+	printf("data: %s\n", buf);
+	return 0;
 }
 
 int recv_http(int sockfd, char **data, size_t *data_len)
@@ -140,11 +206,18 @@ int recv_http(int sockfd, char **data, size_t *data_len)
 				result_size);
 			break;
 		}
-		printf("read %d bytes (%ld total)\n%s\n",
-		       ret, result_size + ret, data_ptr);
+		result_size += ret;
+		if (parse_http(result, result_size) == 0) {
+			memset(result, 0, result_size);
+			data_ptr = result;
+			data_left = result_size;
+			result_size = 0;
+			continue;
+		}
+		printf("read %d bytes (%ld total)\n",
+		       ret, result_size + ret);
 		data_left -= ret;
 		data_ptr += ret;
-		result_size += ret;
 		if (data_left)
 			continue;
 
@@ -167,30 +240,45 @@ int recv_http(int sockfd, char **data, size_t *data_len)
 	return ret;
 }
 
+char *http_header =
+	"POST /v3/watch HTTP/1.1\r\n"
+	"Host: %s:%s\r\n"
+	"Accept: */*\r\n"
+	"Content-Type: application/json\r\n"
+	"Content-Length: %d\r\n\r\n";
+
 int main(int argc, char **argv)
 {
-	int sockfd, flags, ret;
-	size_t buflen;
-	char *buf;
+	int sockfd, flags, ret, hdrlen;
+	size_t buflen, postlen;
+	char *hdr, *buf, *post;
 
-	sockfd = etcd_connect();
+	sockfd = etcd_connect(default_hostname, default_port);
 	if (sockfd < 0)
 		return 1;
 
 	flags = fcntl(sockfd, F_GETFL);
 	fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
-	printf("sending http header (%ld bytes)\n",
-	       strlen(http_header));
-	ret = send_http(sockfd, http_header, strlen(http_header));
+	post = format_watch("nofuse/ports", 0, 0);
+	postlen = strlen(post);
+
+	hdrlen = asprintf(&hdr, http_header,
+		       default_hostname, default_port,
+		       postlen);
+	if (hdrlen < 0)
+		return 1;
+
+	printf("sending http header (%d bytes)\n", hdrlen);
+	ret = send_http(sockfd, hdr, hdrlen);
+	free(hdr);
 	if (ret != 0) {
 		printf("error sending http header, %d bytes left\n", ret);
 		close(sockfd);
 		return 1;
 	}
-	printf("sending http post (%ld bytes)\n",
-	       strlen(http_post_watch));
-	ret = send_http(sockfd, http_post_watch, strlen(http_post_watch));
+	printf("sending http post (%ld bytes)\n", postlen);
+	ret = send_http(sockfd, post, postlen);
 	if (ret != 0) {
 		printf("error sending http post, %d bytes left\n", ret);
 		close(sockfd);
