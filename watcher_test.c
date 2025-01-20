599a2f51627550a0997af785eb5501e9a3ec674e
Hannes Reinecke <hare@suse.de>
2025-01-20 17:37:19 +0100

    watcher_test: improve nvmet callback

--- watcher_test.c
+++ watcher_test.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <limits.h>
 #include <netdb.h>
 #include <errno.h>
@@ -39,20 +40,83 @@ static void update_nvmetd(void *arg, struct etcd_kv *kv)
 		       kv->key, kv->value);
 
 	attr = kv->key + strlen(ctx->prefix) + 1;
-	ret = asprintf(&path, "/sys/kernel/config/nvmet/%s", attr);
+	if (!strncmp(attr, "ports/", 6)) {
+		char *p = attr + 6, *node_name = ctx->node_name;
+
+		if (!node_name)
+			node_name = "localhost";
+		if (strncmp(p, ctx->node_name, strlen(ctx->node_name))) {
+			printf("%s: ignoring foreign port %s\n",
+			       __func__, attr);
+			return;
+		}
+		p = strchr(attr, ':');
+		if (!p) {
+			printf("%s: invalid port %s\n",
+			       __func__, attr);
+			return;
+		}
+		ret = asprintf(&path, "/sys/kernel/config/nvmet/ports/%s",
+			       p + 1);
+	} else
+		ret = asprintf(&path, "/sys/kernel/config/nvmet/%s", attr);
 	if (ret < 0) {
 		return;
 	}
 	ret = stat(path, &st);
 	if (ret < 0) {
-		if (errno != ENOENT && !kv->deleted)
+		char *parent, *ptr;
+
+		if (errno != ENOENT) {
 			printf("%s: error %d accessing %s\n",
 			       __func__, errno, path);
-		return;
+			return;
+		}
+		if (kv->deleted)
+			/* KV deleted and path not present, all done */
+			return;
+		/* Trying to create parent */
+		parent = strdup(path);
+		ptr = strrchr(parent, '/');
+		if (!ptr) {
+			printf("%s: invalid parent %s\n",
+			       __func__, parent);
+			return;
+		}
+		*ptr = '\0';
+		ret = stat(parent, &st);
+		if (ret < 0) {
+			if (errno != ENOENT) {
+				printf("%s: error %d accessing parent %s\n",
+				       __func__, errno, parent);
+				return;
+			}
+			ret = mkdir(parent, 0755);
+			if (ret < 0) {
+				printf("%s: error %d creating parent %s\n",
+				       __func__, errno, parent);
+				return;
+			}
+			ret = stat(parent, &st);
+			if (ret < 0) {
+				printf("%s: error %d accessing new parent %s\n",
+				       __func__, errno, parent);
+				return;
+			}
+		}
+		if ((st.st_mode & S_IFMT) != S_IFDIR) {
+			printf("%s: parent %s is not a directory\n",
+			       __func__, parent);
+			return;
+		}
 	}
 	if (kv->deleted) {
 		printf("%s: delete %s\n",
 		       __func__, path);
+		return;
+	}
+	if (!kv->value) {
+		printf("%s: no value\n", __func__);
 		return;
 	}
 	fd = open(path, O_RDONLY);
