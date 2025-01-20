c0e4badcbbcba0cb0efffd6246462169d30c236b
Hannes Reinecke <hare@suse.de>
2025-01-20 16:06:13 +0100

    watcher_test: check nvmet paths

--- watcher_test.c
+++ watcher_test.c
@@ -21,16 +21,55 @@
 #include "etcd_client.h"
 
 bool etcd_debug = true;
-bool http_debug = true;
+bool http_debug = false;
 int stopped = 0;
 
 static void update_nvmetd(void *arg, struct etcd_kv *kv)
 {
 	struct etcd_ctx *ctx = arg;
+	struct stat st;
+	char *attr, *path;
+	int fd, ret;
 
-	printf("%s: %s key %s value %s\n", __func__,
-	       kv->deleted ? "delete" : "add",
-	       kv->key, kv->value);
+	if (kv->deleted)
+		printf("%s: delete key %s\n",
+		       __func__, kv->key);
+	else
+		printf("%s: add key %s value %s\n", __func__,
+		       kv->key, kv->value);
+
+	attr = kv->key + strlen(ctx->prefix) + 1;
+	ret = asprintf(&path, "/sys/kernel/config/nvmet/%s", attr);
+	if (ret < 0) {
+		return;
+	}
+	ret = stat(path, &st);
+	if (ret < 0) {
+		if (errno != ENOENT && !kv->deleted)
+			printf("%s: error %d accessing %s\n",
+			       __func__, errno, path);
+		return;
+	}
+	if (kv->deleted) {
+		printf("%s: delete %s\n",
+		       __func__, path);
+		return;
+	}
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		printf("%s: error opening %s\n",
+		       __func__, path);
+	} else {
+		char buf[256];
+
+		ret = read(fd, buf, 256);
+		if (ret < 0) {
+			printf("error reading %s\n", path);
+		} else if (strncmp(kv->value, buf, strlen(kv->value)))
+			printf("update from %s to %s\n", buf, kv->value);
+		close(fd);
+	}
+	free(path);
 }
 
 int main(int argc, char **argv)
@@ -47,8 +86,9 @@ int main(int argc, char **argv)
 
 	memset(&ev, 0, sizeof(ev));
 	ev.watch_cb = update_nvmetd;
+	ev.watch_arg = ctx;
 
-	ret = etcd_kv_watch(conn, "nofuse/ports", &ev, 0);
+	ret = etcd_kv_watch(conn, "nofuse", &ev, 0);
 
 	while (ret >= 0) {
 		ret = etcd_kv_watch_continue(conn, &ev);
